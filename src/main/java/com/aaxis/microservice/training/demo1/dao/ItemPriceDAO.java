8b8cf5dba995f24432d546e60100606664096904
scorityzhu <scorityzhu@aaxischina.com>
2018-10-07 18:05:52 +0800

    change initData

--- src/main/java/com/aaxis/microservice/training/demo1/dao/ItemPriceDAO.java
+++ src/main/java/com/aaxis/microservice/training/demo1/dao/ItemPriceDAO.java
@@ -2,12 +2,16 @@ package com.aaxis.microservice.training.demo1.dao;
 
 import com.aaxis.microservice.training.demo1.domain.ItemPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ItemPriceDAO extends JpaRepository<ItemPrice,String> {
 
-    @Query(value = "select id from Product", nativeQuery = true )
-    public List<String> findAllProductIds();
+    @Modifying
+    @Transactional
+    @Query(value = "insert into item_price(id, price) select id,ROUND(rand(1000)*1000,2) from product",nativeQuery = true)
+    int insertItemPrice();
 }
--- src/main/java/com/aaxis/microservice/training/demo1/service/ItemPriceService.java
+++ src/main/java/com/aaxis/microservice/training/demo1/service/ItemPriceService.java
@@ -5,27 +5,13 @@ import com.aaxis.microservice.training.demo1.domain.ItemPrice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @Component
 public class ItemPriceService {
 
     @Autowired
     private ItemPriceDAO mItemPriceDAO;
-
-    public void initData(){
-        List<String> ids = mItemPriceDAO.findAllProductIds();
-
-        for(String productId : ids){
-            ItemPrice itemPrice = new ItemPrice();
-            itemPrice.setId(productId);
-            itemPrice.setPrice(new BigDecimal(new Random().nextDouble() * 1000).setScale(2,   BigDecimal.ROUND_HALF_UP).doubleValue());
-            mItemPriceDAO.save(itemPrice);
-        }
-    }
 
     public ItemPrice findItemPriceById(String pProductId){
         Optional<ItemPrice> optionalItemPrice = mItemPriceDAO.findById(pProductId);
@@ -34,4 +20,9 @@ public class ItemPriceService {
         }
         return null;
     }
+
+    public void initData(){
+        mItemPriceDAO.insertItemPrice();
+    }
+
 }
